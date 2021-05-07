69439930f04c6c9018136dcfe866e757213b5f9a
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-05-07 01:01:16 -0500

    Set up database connection with mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+// Using async for promise
+const connectDB =  async () => {
+    try {
+        const conn =  await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold)
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.bold)
+        // 1 - exit with failure
+        process.exit(1)
+    
+    }
+}
+
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 // ESM style imports
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 // Import of a file requires .js extension
 import products from './data/products.js'
 
 // Config will read .env file, parse the contents
 dotenv.config()
+
+connectDB()
 
 // Initialize express
 const app = express()
@@ -29,4 +33,4 @@ app.get('/api/products/:id', (req, res) => {
 // Pull port from the env var or if not found use 5000
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}.`))
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}.`.yellow.bold))
