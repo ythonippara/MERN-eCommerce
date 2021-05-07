62af68c33a65947434173b891d972d67a20a8f16
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-05-06 23:44:32 -0500

    Pull product data from the backend server with axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+    // products - state name, setproducts - function to manipulate products
+    // Then pass an empty array to useState
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            // De-structuring allows to use data variable directly
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+
+        fetchProducts()
+        // Pass in array of dependencies to fire off side effects??
+    }, [])
+
     return (
         <>
         <h1>Latest Products</h1>
