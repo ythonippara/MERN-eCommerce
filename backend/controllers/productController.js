87276a9e7d0de271af9855db8eaed7162cb8824c
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-07 21:45:35 -0500

    Created Product Controller, Separated Routes and Controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,35 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+    // Use Product model pass empty object will the data
+    const products = await Product.find({})
+    // Test error message for videos 28 and 29
+    //res.status(401)
+    //throw new Error ('Not Authorized!')
+    // Serve json array
+    res.json(products)
+})
+
+// @desc    Fetch a single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        res.json(product)
+    } else {
+        // res.status(404).json({ message: 'Product not found' })
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+export {
+    getProducts,
+    getProductById
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,9 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 // API routes
-
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-    // Use Product model pass empty object will the data
-    const products = await Product.find({})
-    // Test error message for videos 28 and 29
-    //res.status(401)
-    //throw new Error ('Not Authorized!')
-    // Serve json array
-    res.json(products)
-}))
-
-// @desc    Fetch a single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product) {
-        res.json(product)
-    } else {
-        // res.status(404).json({ message: 'Product not found' })
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
