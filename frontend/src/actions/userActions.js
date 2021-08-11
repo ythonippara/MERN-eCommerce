a0712c11c849dc43824fcce9b98fd3cc9e0675ea
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-10 23:46:17 -0500

    Implemented Logout Function, Added User Profile Dropdown to Navbar

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -37,4 +37,13 @@ export const login = (email, password) => async (dispatch) => {
                 : error.message
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    // Remove user info from local storage
+    localStorage.removeItem('userInfo')
+
+    dispatch({
+        type: USER_LOGOUT
+    })
 }
