4483c2eb6d4c5babbc9f0cf7a7db7a3a91da4782
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-12 18:23:58 -0500

    Finished Place Order Handler

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
 
-const PlaceOrderScreen = () => {
-    //const dispatch = useDispatch()
+const PlaceOrderScreen = ({ history }) => {
+    const dispatch = useDispatch()
+
     const cart = useSelector(state => state.cart)
 
     // Show 2 decimals
@@ -18,14 +20,34 @@ const PlaceOrderScreen = () => {
     cart.itemsPrice = addDecimals(cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
     cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
     cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+    // Not working????
     cart.totalPrice = (
         Number(cart.itemsPrice) + 
         Number(cart.shippingPrice) + 
         Number(cart.taxPrice)
     ).toFixed(2)
+
+    const orderCreate = useSelector(state => state.orderCreate)
+    const { order, success, error } = orderCreate
+
+    useEffect(() => {
+        if(success) {
+            history.push(`/order/${order._id}`)
+        }
+        // Ignore the fact that order._id doesn't exist yet...
+        // eslint-disable-next-line
+    }, [history, success])
     
     const placeOrderHandler = () => {
-        console.log('Place Order')
+        dispatch(createOrder({
+            orderItems: cart.cartItems,
+            shippingAddress: cart.shippingAddress,
+            paymentMethod: cart.paymentMethod,
+            itemsPrice: cart.itemsPrice,
+            shippingPrice: cart.shippingPrice,
+            taxPrice: cart.taxPrice,
+            totalPrice: cart.totalPrice,
+        }))
     }
 
     return (
@@ -115,6 +137,9 @@ const PlaceOrderScreen = () => {
                                     <Col>Total</Col>
                                     <Col>${cart.totalPrice}</Col>
                                 </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {error && <Message variant='danger'>{error}</Message>}
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <Button 
