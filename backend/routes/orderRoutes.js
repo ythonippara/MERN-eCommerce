beefadb2bcf08ea512eada1518cd9e057aeef8de
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-13 17:44:09 -0500

    Added getOrderById Route to Backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 // API routes
 router.route('/').post(protect, addOrderItems)
+// This route has to be placed at the bottom
+router.route('/:id').get(protect, getOrderById)
 
 export default router
