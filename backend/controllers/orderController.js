9a68d5ff03d6be410ae7539a5aeefa610b8a105d
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-14 15:58:28 -0500

    Added a Route to Update Order Status to "Paid"

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -56,4 +56,37 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems, getOrderById }
+// @desc    Update order status to "paid"
+// @route   PUT /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    // Get order ID from the URL using req.params.id
+    // Research populate() !!!
+    const order = await Order.findById(req.params.id)
+
+    if(order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        // Comes from PayPal
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            // Comes from Payer object
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status('404')
+        throw new Error('Order not found')
+    }
+})
+
+export { 
+    addOrderItems, 
+    getOrderById, 
+    updateOrderToPaid,
+}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderController.js'
+import { 
+    addOrderItems, 
+    getOrderById,
+    updateOrderToPaid
+} from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 // API routes
 router.route('/').post(protect, addOrderItems)
 // This route has to be placed at the bottom
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 export default router
