a09d3f368055eb2e5829f663d6c200b86d8579ae
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-15 14:28:20 -0500

    Created Delete User Screen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listUsers } from '../actions/userActions'
+import { listUsers, deleteUser } from '../actions/userActions'
 
 const UserListScreen = ({ history }) => {
     const dispatch = useDispatch()
@@ -15,6 +15,9 @@ const UserListScreen = ({ history }) => {
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
+    const userDelete = useSelector(state => state.userDelete)
+    const { success: successDelete } = userDelete
+
     useEffect(() => {
         if(userInfo && userInfo.isAdmin) {
             dispatch(listUsers())  
@@ -22,11 +25,14 @@ const UserListScreen = ({ history }) => {
             history.push('/login')
         }
         // Dependency array
-    }, [dispatch, history, userInfo])
+    }, [dispatch, history, userInfo, successDelete])
 
       // Handler
       const deleteHandler = (id) => {
-        console.log('Delete')
+          // How to style this??
+          if(window.confirm('Are you sure?')) {
+              dispatch(deleteUser(id))
+          }
     }
 
     return (
