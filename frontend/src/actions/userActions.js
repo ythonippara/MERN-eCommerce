384eb84e4b734a03aa34716b9f29c58976cbc84e
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-15 10:27:51 -0500

    Added Constants, Reducer & Action for Listing All Users

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,7 +13,10 @@ import {
     USER_REGISTER_SUCCESS, 
     USER_UPDATE_PROFILE_FAIL, 
     USER_UPDATE_PROFILE_REQUEST,
-    USER_UPDATE_PROFILE_SUCCESS
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -183,6 +186,40 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({ 
             type: USER_UPDATE_PROFILE_FAIL,
+            payload: 
+              error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message
+        })
+    }
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_LIST_REQUEST
+        })
+
+        // Get access to the logged in user object
+        const { userLogin: { userInfo } } = getState()
+        
+        const config = {
+            headers: {
+                // Get token with getState and pass in headers
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get('/api/users', config)
+
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload: data
+        })
+        
+    } catch (error) {
+        dispatch({ 
+            type: USER_LIST_FAIL,
             payload: 
               error.response && error.response.data.message 
                 ? error.response.data.message 
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,13 +13,15 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
-    USER_UPDATE_PROFILE_RESET
+    USER_UPDATE_PROFILE_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL
 } from '../constants/userConstants'
 
 // User Login Reducer
 // Reducer takes a state (empty object) and an action
 export const userLoginReducer = (state = { }, action) => {
-
     // Use switch to evaluate type in the action object
     switch (action.type) {
         case USER_LOGIN_REQUEST:
@@ -36,8 +38,6 @@ export const userLoginReducer = (state = { }, action) => {
 }
 
 export const userRegisterReducer = (state = { }, action) => {
-    
-    // Use switch to evaluate type in the action object
     switch (action.type) {
         case USER_REGISTER_REQUEST:
             return { loading: true }
@@ -51,8 +51,6 @@ export const userRegisterReducer = (state = { }, action) => {
 }
 
 export const userDetailsReducer = (state = { user: {} }, action) => {
-    
-    // Use switch to evaluate type in the action object
     switch (action.type) {
         case USER_DETAILS_REQUEST:
             return { ...state, loading: true }
@@ -68,8 +66,6 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
 }
 
 export const userUpdateProfileReducer = (state = { }, action) => {
-    
-    // Use switch to evaluate type in the action object
     switch (action.type) {
         case USER_UPDATE_PROFILE_REQUEST:
             return { loading: true }
@@ -80,6 +76,19 @@ export const userUpdateProfileReducer = (state = { }, action) => {
         // Video 91 Bug Fixes    
         case USER_UPDATE_PROFILE_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const userListReducer = (state = { users: [] }, action) => {
+    switch (action.type) {
+        case USER_LIST_REQUEST:
+            return { loading: true }
+        case USER_LIST_SUCCESS:
+            return { loading: false, users: action.payload }
+        case USER_LIST_FAIL:
+            return { loading: false, error: action.payload }
         default:
             return state
     }
