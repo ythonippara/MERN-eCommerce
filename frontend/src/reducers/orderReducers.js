77ebeb28e4c16bb559c429ad5d55d14173525fc0
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-14 20:53:27 -0500

    Added My Orders Table

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -97,7 +97,7 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
         case ORDER_LIST_MY_SUCCESS:
             return { 
                 loading: false, 
-                orders: action.payload,
+                orders: action.payload
             }
         case ORDER_LIST_MY_FAIL:
             return { 
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Table, Form, Button, Row, Col } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { listMyOrders } from '../actions/orderActions'
 import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = ({ location, history }) => {
@@ -26,6 +28,9 @@ const ProfileScreen = ({ location, history }) => {
     const userUpdateProfile = useSelector(state => state.userUpdateProfile)
     const { success } = userUpdateProfile
 
+    const orderListMy = useSelector(state => state.orderListMy)
+    const { loading:loadingOrders, error:errorOrders, orders } = orderListMy
+
     // Redirect if user is already logged in
     useEffect(() => {
         // Check for user info
@@ -36,6 +41,7 @@ const ProfileScreen = ({ location, history }) => {
                 // Video 91 Bug Fixes
                 dispatch({ type: USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'))
+                dispatch(listMyOrders)
             } else {
                 setName(user.name)
                 setEmail(user.email)
@@ -113,6 +119,64 @@ const ProfileScreen = ({ location, history }) => {
           </Col>
           <Col md={9}>
               <h2>My Orders</h2>
+              {loadingOrders ?
+                <Loader />
+              : errorOrders ? <Message variant='danger'>{errorOrders}</Message>
+              : (
+                  <Table 
+                    striped 
+                    bordered 
+                    hover 
+                    responsive
+                    className='table-sm'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>DATE</th>
+                                <th>TOTAL</th>
+                                <th>PAID</th>
+                                <th>DELIVERED</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {orders.map(order => (
+                                <tr key={order._id}>
+                                    {/** createdAt or paidAt?? 
+                                     * Substring the first 10 char : 0,9???
+                                    */}
+                                    <td>{order.createdAt.substring(0, 10)}</td>
+                                    <td>{order.totalPrice}</td>
+                                    <td>
+                                        {order.isPaid ? (
+                                            order.paidAt.substring(0, 10)
+                                        ) : (
+                                            <i 
+                                                className='fas fa-times' 
+                                                style={{color: 'red'}}
+                                            />
+                                        )}
+                                    </td>
+                                    <td>
+                                        {order.isDelivered ? (
+                                            order.deliveredAt.substring(0, 10)
+                                        ) : (
+                                            <i 
+                                                className='fas fa-times' 
+                                                style={{color: 'red'}}
+                                            />
+                                        )}
+                                    </td>
+                                    <td>
+                                        <LinkContainer to={`/order/${order._id}`}>
+                                            <Button className='btn-sm' variant='light'>Details</Button>
+                                        </LinkContainer>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+              )}
           </Col>
       </Row>
     )
