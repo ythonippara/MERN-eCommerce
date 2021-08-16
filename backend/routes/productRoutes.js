d0c74139a2a979d67227e0d86162b04a17e2068f
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-15 23:06:16 -0500

    Added Backend Routes to Create & Update Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,15 +3,18 @@ const router = express.Router()
 import { 
     getProducts, 
     getProductById, 
-    deleteProduct 
+    deleteProduct,
+    createProduct,
+    updateProduct,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 // API routes
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 router
     .route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 export default router
