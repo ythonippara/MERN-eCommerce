c4a8aff063e5b3b3cdd331a0f038d22ae84e26e2
Yulia Thonippara <ythonippara@hawk.iit.edu>
2021-08-17 15:40:17 -0500

    Added Constants, Reducer & Action  for Review Prod

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,6 +15,9 @@ import {
     PRODUCT_UPDATE_REQUEST,
     PRODUCT_UPDATE_SUCCESS,
     PRODUCT_UPDATE_FAIL,
+    PRODUCT_CREATE_REVIEW_REQUEST,
+    PRODUCT_CREATE_REVIEW_SUCCESS,
+    PRODUCT_CREATE_REVIEW_FAIL,
 } from '../constants/productConstants'
 
 // Actions
@@ -142,6 +145,37 @@ export const updateProduct = (product) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({ 
             type: PRODUCT_UPDATE_FAIL,
+            payload: 
+              error.response && error.response.data.message 
+                ? error.response.data.message 
+                : error.message
+        })
+    }
+}
+
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PRODUCT_CREATE_REVIEW_REQUEST })
+
+        // Get access to the logged in user object
+        const { userLogin: { userInfo } } = getState()
+        
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                // Get token with getState and pass in headers
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        
+        // Put into variable if message is needed
+        await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+        dispatch({ type: PRODUCT_CREATE_REVIEW_SUCCESS })
+
+    } catch (error) {
+        dispatch({ 
+            type: PRODUCT_CREATE_REVIEW_FAIL,
             payload: 
               error.response && error.response.data.message 
                 ? error.response.data.message 
